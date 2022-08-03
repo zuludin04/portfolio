68fc798e37f3a5c976ac75a6d76a3ae31209b244
zuludin <zmauludin@bwa.id>
2022-08-03 12:51:57 +0700

    fixing detail page

--- lib/ui/project_detail_screen.dart
+++ lib/ui/project_detail_screen.dart
@@ -14,139 +14,137 @@ class ProjectDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Obx(
-      () => Scaffold(
-        backgroundColor: const Color(0xff111111),
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: const Color(0xff111111),
-              leading: IconButton(
-                onPressed: () => Navigator.pop(context),
-                icon: const Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                ),
-              ),
-              expandedHeight: size.height * 0.7,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Image.asset(
-                  project.image,
-                  fit: BoxFit.cover,
-                ),
+    return Scaffold(
+      backgroundColor: const Color(0xff111111),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: const Color(0xff111111),
+            leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
               ),
             ),
-            SliverPadding(
-              padding: EdgeInsets.symmetric(
-                vertical: 72,
-                horizontal: ResponsiveWidget.isLargeScreen(context)
-                    ? size.width * 0.18
-                    : ResponsiveWidget.isMediumScreen(context)
-                        ? size.width * 0.15
-                        : size.width * 0.12,
-              ),
-              sliver: SliverToBoxAdapter(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SectionTitle(
-                      title: project.name,
-                      color: Colors.white,
-                    ),
-                    SelectableText(
-                      project.description,
-                      style: const TextStyle(
-                        fontFamily: 'Montserrat Regular',
-                        fontSize: 18,
-                        letterSpacing: 1,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-                    ProjectInformation(
-                      title: 'Platform',
-                      info: 'Android',
-                      url: project.link,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(height: 32),
-                    ProjectInformation(
-                      title: 'Technology',
-                      info: project.techs[1].name,
-                      url: project.link,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(height: 32),
-                    ProjectInformation(
-                      title: 'Company',
-                      info: project.company,
-                      url: project.link,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(height: 32),
-                    ProjectInformation(
-                      title: 'Status',
-                      info: project.status,
-                      url: project.link,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
+            expandedHeight: size.height * 0.7,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                project.image,
+                fit: BoxFit.cover,
               ),
             ),
-            SliverPadding(
-              padding: EdgeInsets.symmetric(
-                horizontal: ResponsiveWidget.isLargeScreen(context)
-                    ? size.width * 0.18
-                    : ResponsiveWidget.isMediumScreen(context)
-                        ? size.width * 0.15
-                        : size.width * 0.12,
-              ),
-              sliver: SliverToBoxAdapter(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SectionTitle(
-                      title: 'Screenshot',
-                      color: Colors.white,
-                    ),
-                    Wrap(
-                      children: project.screenshots
-                          .map((e) => ProjectScreenshot(assets: e))
-                          .toList(),
-                    ),
-                    const SizedBox(height: 72),
-                  ],
-                ),
-              ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              vertical: 72,
+              horizontal: ResponsiveWidget.isLargeScreen(context)
+                  ? size.width * 0.18
+                  : ResponsiveWidget.isMediumScreen(context)
+                      ? size.width * 0.15
+                      : size.width * 0.12,
             ),
-            SliverToBoxAdapter(
-              child: Container(
-                color: const Color(0xff04363D),
-                padding: const EdgeInsets.all(8),
-                alignment: Alignment.center,
-                child: const SelectableText.rich(
-                  TextSpan(
-                    text: 'Made by ',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat Regular',
-                      color: Colors.white,
-                      letterSpacing: 2,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: 'Zulfikar Mauludin',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
+            sliver: SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SectionTitle(
+                    title: project.name,
+                    color: Colors.white,
                   ),
+                  SelectableText(
+                    project.description,
+                    style: const TextStyle(
+                      fontFamily: 'Montserrat Regular',
+                      fontSize: 18,
+                      letterSpacing: 1,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  ProjectInformation(
+                    title: 'Platform',
+                    info: 'Android',
+                    url: project.link,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 32),
+                  ProjectInformation(
+                    title: 'Technology',
+                    info: project.techs[1].name,
+                    url: project.link,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 32),
+                  ProjectInformation(
+                    title: 'Company',
+                    info: project.company,
+                    url: project.link,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 32),
+                  ProjectInformation(
+                    title: 'Status',
+                    info: project.status,
+                    url: project.link,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: ResponsiveWidget.isLargeScreen(context)
+                  ? size.width * 0.18
+                  : ResponsiveWidget.isMediumScreen(context)
+                      ? size.width * 0.15
+                      : size.width * 0.12,
+            ),
+            sliver: SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SectionTitle(
+                    title: 'Screenshot',
+                    color: Colors.white,
+                  ),
+                  Wrap(
+                    children: project.screenshots
+                        .map((e) => ProjectScreenshot(assets: e))
+                        .toList(),
+                  ),
+                  const SizedBox(height: 72),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              color: const Color(0xff04363D),
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              child: const SelectableText.rich(
+                TextSpan(
+                  text: 'Made by ',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat Regular',
+                    color: Colors.white,
+                    letterSpacing: 2,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Zulfikar Mauludin',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
