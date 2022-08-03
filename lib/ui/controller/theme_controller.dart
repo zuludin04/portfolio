c1c52235130826e2bb82ade6c63df488cbfc429b
zuludin <zmauludin@bwa.id>
2022-08-03 12:18:28 +0700

    remove theme controller

--- lib/ui/controller/theme_controller.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:get/get.dart';
-
-// class ThemeController extends GetxController {
-//   var isDark = true.obs;
-
-//   void changeTheme() {
-//     isDark.value = !isDark.value;
-//   }
-// }
