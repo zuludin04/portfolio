3497b6acffd084490017870ab43cd62b39223b3d
zuludin <zmauludin04@gmail.com>
2025-01-13 14:30:46 +0700

    feat: add app navigation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
 import ProjectDetailPage from "./pages/ProjectDetailPage";
 import { AnimatePresence } from "framer-motion";
+import Navigation from "./components/Navigation";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="portfolio-app">
+      <header>
+        <img src={logo} width={48} />
+        <Navigation />
+      </header>
       <main>
         <AnimatePresence mode="wait">
           <Routes>
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,28 @@
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <button className="menu-link">Home</button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button className="menu-link">About</button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button className="menu-link">Project</button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button className="menu-link">Contact</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
